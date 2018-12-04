4e5636343632dafbdfdc0921550bb6386c432fb4
Rafał Postek <rpostek94@gmail.com>
2018-12-04 18:02:55 +0100

    MVVM

---
+++ ArmyBase/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using ArmyBase.ViewModel;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ArmyBase
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            //HibernatingRhinos.Profiler.Appender.EntityFramework.EntityFrameworkProfiler.Initialize();
+            Initialize();
+
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<StartUpViewModel>();
+
+        }
+    }
+}
